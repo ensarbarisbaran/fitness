325d00d2633d92c309278482c3e05e32edb10c8a
Alperen Kilinc <kilincfurkanalperen@gmail.com>
2024-05-15 15:34:43 +0300

    Sayfa bilesenleri taslagi olusturuldu

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Home from './pages/Home'
 
 const App = () => {
   return (
@@ -6,7 +7,9 @@ const App = () => {
       <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
-      
+    <div>
+    <Home/>
+    </div>
     </div>
   )
 }

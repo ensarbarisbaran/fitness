f695d91d01f3c6126aed787cdea7676e4b820c6b
ENSAR BARIŞ BARAN <78018023+ensarbarisbaran@users.noreply.github.com>
2024-05-22 12:52:46 +0300

    Login Pge
    Login Page tamamlandı

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,28 @@
 import React from 'react'
 import Home from './pages/Home'
 import Navbar from './partials/Navbar'
-import Login from './pages/Login'
+import Login from "./pages/Login"
+import { BiMessageRoundedDots } from 'react-icons/bi'
+import Register from './pages/Register'
+
 
 
 
 const App = () => {
   return (
     <body>
-      <div className="bg-slate-900">
+      <div className="bg-slate-900" >
         <Navbar />
         <div>
           <Home />
         </div>
 
       </div>
-      <div className=' bg-green-400
-       text-white g-[100vh] flex justify-center items-center bg-cover'>
+      <div className='  text-white h-[100vh] flex justify-center items-center bg-cover' style={{ "backgroundImage": "url('../src/assets/bg.png" }}>
         <Login />
-
+      </div>
+      <div className='  text-white h-[100vh] flex justify-center items-center bg-cover' style={{ "backgroundImage": "url('../src/assets/bg.png" }}>
+        <Register />
       </div>
     </body>
   )
